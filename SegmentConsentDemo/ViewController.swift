5f3f766129db0d14dd85a189b245692e700b8493
Brandon Sneed <brandon.sneed@segment.com>
2019-09-16 09:24:54 -0700

    Initial Commit

---
+++ SegmentConsentDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegmentConsentDemo
+//
+//  Created by Brandon Sneed on 9/16/19.
+//  Copyright © 2019 Brandon Sneed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
